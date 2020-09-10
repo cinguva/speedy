173b0d929a2d10fb77b70a66efad88e1350d4c9f
chanderinguva <55096640+chanderinguva@users.noreply.github.com>
2020-09-10 09:25:14 -0500

    Create search.py

---
+++ search.py
@@ -0,0 +1,11 @@
+import re 
+  
+def find(string): 
+  
+    # findall() has been used  
+    # with valid conditions for urls in string 
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)       
+    return [x[0] for x in url if "https://<tenant>.zendesk.com/agent/tickets/" in x[0] ] 
+      
+
